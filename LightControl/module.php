e3ae3c6f092d70d304b1f743d5efa98d1eb32713
Franklin van Velthuizen <franklinvv@yoki.org>
2020-10-04 10:46:33 +0200

    Gradually changing intensity

--- LightControl/module.php
+++ LightControl/module.php
@@ -10,21 +10,16 @@
 			$this->RegisterPropertyInteger("BottomSwitchValue", 0);
 			$this->RegisterPropertyInteger("TopSwitchValue", 0);
 			$this->RegisterPropertyInteger("IlluminationValue", 0);
-			$this->RegisterPropertyString("Variables", "");
+			$this->RegisterPropertyString("Instances", "");
 
-			$this->RegisterVariableFloat("CurrentDimValue", "Current dim value", "", 0);
-
-			$var = IPS_GetObjectIDByIdent("CurrentDimValue", $this->InstanceID);
-			if($var) {
-				$this->SetValue("CurrentDimValue", 0.5);
-			}
+			$this->RegisterVariableFloat("CurrentFloatValue", "Current float value", "", 0);
+//			$var = IPS_GetObjectIDByIdent("CurrentFloatValue", $this->InstanceID);
 
 			$this->RegisterVariableBoolean("CurrentSwitchValue", "Current switch value", "~Switch", true);
+//			$var = IPS_GetObjectIDByIdent("CurrentSwitchValue", $this->InstanceID);
 
-			$var = IPS_GetObjectIDByIdent("CurrentSwitchValue", $this->InstanceID);
-			if($var) {
-				$this->SetValue("CurrentSwitchValue", true);
-			}
+			$this->RegisterVariableInteger("CurrentIntegerValue", "Current integer value", "", 0);
+//			$var = IPS_GetObjectIDByIdent("CurrentIntegerValue", $this->InstanceID);
 		}
 
 		public function ApplyChanges() {
@@ -45,32 +40,43 @@
 
 			$illumination = GetValueInteger($illuminationValueSource);
 
-			$dimValue = $this->calculateDimValue($illumination);
-			$this->SetValue("CurrentDimValue", $dimValue);
+			$floatValue = $this->calculateFloatValue($illumination);
+			$this->SetValue("CurrentFloatValue", $floatValue);
 
-			$switchValue = $this->calculateSwitchValue($illumination);
-			$this->SetValue("CurrentSwitchValue", $switchValue);
+			$boolValue = $this->calculateSwitchValue($illumination);
+			$this->SetValue("CurrentSwitchValue", $boolValue);
 
-			$integerValue = $dimValue * 255;
-			$floatValue = $dimValue;
-			$boolValue = $switchValue;
+			$integerValue = $this->calculateIntegerValue($illumination);
+			$this->SetValue("CurrentIntegerValue", $integerValue);
 
-			$variables = $this->getRegisteredVariables();
-			IPS_LogMessage("LightControl", print_r($variables, true));
-			foreach($variables as $variable) {
-				$variable = IPS_GetVariable($variable->VariableID);
-				switch($variable["VariableType"]) {
-					case 0:
-						RequestAction($variable["VariableID"], $boolValue);
-					break;
-					case 1:
-						RequestAction($variable["VariableID"], $integerValue);
-					break;
-					default:
-					case 2:
-						RequestAction($variable["VariableID"], $floatValue);
+			$instances = $this->getRegisteredInstances();
+
+			foreach($instances as $instance) {
+				$instance = IPS_GetInstance($instance->InstanceID);
+				switch($instance["ModuleInfo"]["ModuleName"]) {
+					case "HUELight":
+						//HUE_SetBrightness($instance["InstanceID"], $integerValue);
+						$params = array(
+							"BRIGHTNESS" => $integerValue,
+							"STATE" => ($integerValue > 0),
+							"TRANSITIONTIME" => 200
+						);
+						HUE_SetValues($instance["InstanceID"], $params);
+						IPS_LogMessage("LightControl", $integerValue);
 					break;
 				}
+				// switch($instance["VariableType"]) {
+				// 	case 0:
+				// 		RequestAction($variable["VariableID"], $boolValue);
+				// 	break;
+				// 	case 1:
+				// 		RequestAction($variable["VariableID"], $integerValue);
+				// 	break;
+				// 	default:
+				// 	case 2:
+				// 		RequestAction($variable["VariableID"], $floatValue);
+				// 	break;
+				// }
 			}
 		}
 
@@ -79,22 +85,30 @@
 			parent::Destroy();
 		}
 
-		private function getRegisteredVariables() {
-			$variablesJson = $this->ReadPropertyString("Variables");
-			$result = json_decode($variablesJson);
+		private function getRegisteredInstances() {
+			$instancesJson = $this->ReadPropertyString("Instances");
+			$result = json_decode($instancesJson);
 			return (json_last_error() == JSON_ERROR_NONE) ? $result : NULL;
 		}
 
-		private function calculateDimValue($illuminationValue) {
+		private function calculateFloatValue($illuminationValue) {
 			$bottomDimValue = $this->ReadPropertyInteger("BottomDimValue");
 			$topDimValue = $this->ReadPropertyInteger("TopDimValue");
-			// $bottomSwitchValue = $this->ReadPropertyInteger("BottomSwitchValue");
-			// $topSwitchValue = $this->ReadPropertyInteger("TopSwitchValue");
 
 			$delta = $topDimValue - $bottomDimValue;
 
 			$dimValue = 1 - round(($illuminationValue - $bottomDimValue) / $delta, 1);
 			return min(1.0, max(0.0, $dimValue));
+		}
+
+		private function calculateIntegerValue($illuminationValue) {
+			$bottomDimValue = $this->ReadPropertyInteger("BottomDimValue");
+			$topDimValue = $this->ReadPropertyInteger("TopDimValue");
+
+			$delta = $topDimValue - $bottomDimValue;
+
+			$dimValue = 254 - round((($illuminationValue - $bottomDimValue) / $delta) * 254);
+			return min(254, max(0, $dimValue));
 		}
 
 		private function calculateSwitchValue($illuminationValue) {
